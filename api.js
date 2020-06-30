2fd0d7ef20e38de863b72a4b22c5220179419963
Vladimir Panchenko <vpanchenko@acumatica.com>
2020-06-30 19:59:24 +0300

    Properly handle HTTP errors related to the credentials and access rights - show a meaningful error to the user

--- api.js
+++ api.js
@@ -13,6 +13,21 @@ class JiraApi {
 			}
 		});
 
+		this.api.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				if (error.response.status === 401) {
+					throw Error('Invalid credentials. Please check that your username and password are correct.');
+				} else if (error.response.status === 403) {
+					throw Error(
+						"You do not have access to the entities you're querying. Please contact the JIRA administrator."
+					);
+				} else {
+					return Promise.reject(error);
+				}
+			}
+		);
+
 		// TODO: remove after upgrading axios to 0.20.0
 		// https://github.com/axios/axios/issues/2190
 		this.api.interceptors.request.use((config) => {
